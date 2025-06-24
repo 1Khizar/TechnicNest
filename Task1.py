0273d3d099cf86982c989fc043b5ebef2cabbd46
1Khizar <khizar12006@gmail.com>
2025-06-24 10:30:21 +0500

    first tcommit

---
+++ Task1.py
@@ -0,0 +1,6 @@
+print("Name : Khizar Ishtiaq")
+print("\nAge : 19")
+print("\nHobby : Learning new skills")
+
+print("\nWhy I want tolearn Python ?")
+print("To work on Machine learning and to automtate tasks")
